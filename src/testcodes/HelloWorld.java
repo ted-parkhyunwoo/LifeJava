3bd676752d3d58cb567890c6f49d6a0e4a2e1cdd
ted <zoogree@gmail.com>
2025-09-23 00:51:10 +0900

    init test codes

---
+++ src/testcodes/HelloWorld.java
@@ -0,0 +1,10 @@
+package testcodes;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 5;
+        for (int i = 0; i < a; ++i) {
+            System.out.println("Hello");
+        }
+    }
+}
