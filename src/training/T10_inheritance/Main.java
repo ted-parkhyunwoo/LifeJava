62165d9c38b5729110e248b39696408797d52eaf
ted <zoogree@gmail.com>
2025-09-25 06:28:54 +0900

    T10의 상속기능 기초설명 추가.

---
+++ src/training/T10_inheritance/Main.java
@@ -0,0 +1,23 @@
+package training.T10_inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        SpiderMan 은 Person을 is a관계로 상속받고,
+        Spider의 fireweb()만 내부객체생성하여 사용. (has a 관계)
+        이유는 move()가 양쪽 클래스에 중복되고(모호성), java는 단일상속만 가능하므로.
+
+        +@. python과 cpp는 다중상속 가능: 재점검해볼것.
+        python: MRO와 super()를 따라 호출순서 지정
+        cpp: 가상상속으(virtual inheritance)로 충돌방지
+        java: interface로 우회함(나중에 학습)
+         */
+
+        SpiderMan sman= new SpiderMan("Ted");
+        sman.move();
+        sman.fireweb();
+        System.out.println(sman.name);
+
+    }
+}
