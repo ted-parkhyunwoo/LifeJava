420e80e933b7b72065684e121804c6b9dc4f4f6c
ted <zoogree@gmail.com>
2025-09-25 13:55:15 +0900

    print 단축예제:테스트코드 업데이트 및 적용

---
+++ src/training/module/Print.java
@@ -0,0 +1,9 @@
+package training.module;
+
+public class Print {
+    public static void ln(Object obj) {
+        System.out.println(obj);
+    }
+}
+
+
--- src/training/testcodes/HelloWorld.java
+++ src/training/testcodes/HelloWorld.java
@@ -1,10 +1,11 @@
 package training.testcodes;
+import training.module.Print;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 2;
         for (int i = 0; i < a; ++i) {
-            System.out.println("Hello");
+            Print.ln("HelloWorld");
         }
     }
 }
