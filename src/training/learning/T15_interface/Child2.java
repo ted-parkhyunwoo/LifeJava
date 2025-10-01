9829f612bb2bce0e10916cb2217b48698395244d
ted-desktop-win <zoogree@gmail.com>
2025-10-01 11:42:12 +0900

    T15: 코드 예제 추가(블랭크 파이널 변수)와 주석의 일부 표현 오류 개선.

--- src/training/learning/T15_interface/Child2.java
+++ src/training/learning/T15_interface/Child2.java
@@ -1,11 +1,9 @@
 package training.learning.T15_interface;
 
 public class Child2 extends Parent {
-    private int x = 2;
-    public void func() {
-        System.out.println("child2.");
-    }
-    public String str() {
-        return "Base: " + Base + ", Child: " + x;
-    }
+    Child2() { super(2); }
+
+    // @Override 를 생략하는것은 권장하진 않는다. (의도를 명확히 하기 위함)
+    public void func() { System.out.println("child2."); }
+    public String str() { return "Base: " + Base + ", id: " + id; }
 }
